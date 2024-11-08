d679ebd5c48a7fcc6aa880d276bee319b80395c8
aayushtmG <aiyoustmg@gmail.com>
2024-11-08 19:34:51 +0545

    memory allocation in heap

---
+++ heapmemory.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using std::cin, std::cout, std::endl;
+
+
+int main(){
+   //int x = 20; // created in stack
+    //int *pointer = new int(50); //created in heap
+    int *array = new int[3]; //created in heap
+}
+//notes: 
+// array will point to the memory address of the first array element
+//for(int x = 0 ; x < 3; x++ ){
+//    cout << *(array + x)<<endl; 
+//}
+// thats why i am adding 1 to access the next element from the first of the array
+
+
