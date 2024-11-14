0c94a57d36a00c1101a74172c2c1c281d4182893
aayushtmG <aiyoustmg@gmail.com>
2024-11-14 16:14:27 +0545

    Updated

--- test.cpp
+++ test.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 using std::cout , std::endl;
+void fun(int &x, int &y);
 
 int main(){
     int A[5]{1,2,3,4,5};
-    int num = 5;
+    int x = 20;
+    int &y = x;
+    cout << y<< endl;
+    return 0;
 }
 
