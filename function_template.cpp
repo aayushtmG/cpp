6d6be3073f57fa07e68749c1c6dddca09b0324c2
aayushtmG <aiyoustmg@gmail.com>
2024-11-19 12:50:16 +0545

    Generic or function template

---
+++ function_template.cpp
@@ -0,0 +1,17 @@
+// Generic or function template
+#include <iostream>
+using std::cout,std::endl;
+
+template <class T>
+T max(T a, T b){
+    return a>b ? a : b;
+}
+
+int main(){
+    int a = 20;
+    int b = 30;
+    cout << max(a,b) << " is bigger" << endl;
+    float c = 3.5;
+    float d = 3.2;
+    cout << max(c,d) << " is bigger" << endl;
+}
